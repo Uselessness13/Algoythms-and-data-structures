f4121780ba2f11ea74470ec59737b7828b3aacb2
aaand <aaand@USELESSNOTEBOOK>
2016-12-03 19:52:57 +0300

    Добавьте файлы проекта.

---
+++ Шашечки/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Шашечки
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.history = new System.Windows.Forms.RichTextBox();
+            this.whitesh = new System.Windows.Forms.PictureBox();
+            this.blacksh = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.whitesh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blacksh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // history
+            // 
+            this.history.Location = new System.Drawing.Point(429, 12);
+            this.history.Name = "history";
+            this.history.ReadOnly = true;
+            this.history.Size = new System.Drawing.Size(150, 400);
+            this.history.TabIndex = 0;
+            this.history.Text = "";
+            // 
+            // whitesh
+            // 
+            this.whitesh.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("whitesh.BackgroundImage")));
+            this.whitesh.Image = global::Шашечки.Properties.Resources.white_checker;
+            this.whitesh.Location = new System.Drawing.Point(300, 249);
+            this.whitesh.Name = "whitesh";
+            this.whitesh.Size = new System.Drawing.Size(50, 50);
+            this.whitesh.TabIndex = 1;
+            this.whitesh.TabStop = false;
+            this.whitesh.Visible = false;
+            // 
+            // blacksh
+            // 
+            this.blacksh.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("blacksh.BackgroundImage")));
+            this.blacksh.Image = global::Шашечки.Properties.Resources.black_checker;
+            this.blacksh.Location = new System.Drawing.Point(300, 305);
+            this.blacksh.Name = "blacksh";
+            this.blacksh.Size = new System.Drawing.Size(50, 50);
+            this.blacksh.TabIndex = 2;
+            this.blacksh.TabStop = false;
+            this.blacksh.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 421);
+            this.Controls.Add(this.blacksh);
+            this.Controls.Add(this.whitesh);
+            this.Controls.Add(this.history);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "ШАШЕЧКИ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.whitesh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blacksh)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox history;
+        private System.Windows.Forms.PictureBox whitesh;
+        private System.Windows.Forms.PictureBox blacksh;
+    }
+}
+
---
+++ Шашечки/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Шашечки
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[,] board = new PictureBox[8, 8];
+        private int numberOfStep = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    PictureBox boardpart = new PictureBox();
+                    boardpart.Width = 50;
+                    boardpart.Height = 50;
+                    boardpart.Name = i.ToString() + "_" + j.ToString();
+                    boardpart.Location = new Point((10 + j * 50), (10 + i * 50));
+                    boardpart.Visible = true;
+                    boardpart.BorderStyle = BorderStyle.FixedSingle;
+                    if (i%2 == 0)
+                    {
+                        if (j%2 == 0)
+                        {
+                            boardpart.BackgroundImage = Properties.Resources.white_bg;
+                        }
+                        else
+                        {
+                            boardpart.BackgroundImage = Properties.Resources.black_bg;
+                            if (i <= 2)
+                            {
+                                boardpart.Image = blacksh.Image;
+                            }
+                            if (i > 4)
+                            {
+                                boardpart.Image = whitesh.Image;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (j % 2 != 0)
+                        {
+                            boardpart.BackgroundImage = Properties.Resources.white_bg;
+                        }
+                        else
+                        {
+                            boardpart.BackgroundImage = Properties.Resources.black_bg;
+                            if (i <= 2)
+                            {
+                                boardpart.Image = blacksh.Image;
+                            }
+                            if (i > 4)
+                            {
+                                boardpart.Image = whitesh.Image;
+                            }
+                        }
+                    }
+
+                    boardpart.Click += new System.EventHandler(this.boardpart_Click);
+
+                    board[i, j] = boardpart;
+
+                    Controls.Add(board[i, j]);
+
+                }
+            }
+        }
+
+        private void boardpart_Click(object sender, EventArgs e)
+        {
+            numberOfStep++;
+
+            int y = ((Cursor.Position.X + 20 - this.DesktopLocation.X ) / 50) - 1;
+            int x = ((Cursor.Position.Y - this.DesktopLocation.Y ) / 50) - 1;
+
+            history.Text += (x.ToString() + " ; " + y.ToString()+" | ");
+
+            if (board[x, y].Image == whitesh.Image | board[x, y].Image == blacksh.Image)
+            {
+                string player = numberOfStep % 2 != 0 ? "white" : "black";
+
+                MessageBox.Show(board[x, y].Image.GetType().ToString());
+                
+                if (player == "white" && board[x, y].Image == whitesh.Image)
+                {
+                    MessageBox.Show("Белые");
+                    if (y > 0 && y < 7 && x > 1 && board[x - 1, y - 1].Image!= whitesh.Image && board[x - 1, y + 1].Image != whitesh.Image)
+                    {
+                        MessageBox.Show((x - 1).ToString() + " ; (" + (y - 1).ToString() + ", " + (y + 1).ToString()+")");
+                        board[x - 1, y - 1].BackgroundImage = Properties.Resources.black_highlited;
+                        board[x - 1, y + 1].BackgroundImage = Properties.Resources.black_highlited;
+
+                    }
+                    if (y == 0 && x > 1 && board[x - 1, y + 1].Image != whitesh.Image)
+                    {
+                        board[x - 1, y + 1].BackgroundImage = Properties.Resources.black_highlited;
+                    }
+                    if (y == 7 && x > 1 && board[x - 1, y - 1].Image != whitesh.Image)
+                    {
+                        board[x - 1, y - 1].BackgroundImage = Properties.Resources.black_highlited;
+                    }
+                }
+
+                else
+                {
+                    if (board[x, y].Image == blacksh.Image)
+                    {
+                        MessageBox.Show("Чёрные");
+                        if (y > 0 && y < 7 && x < 7)
+                        {
+                            board[x + 1, y - 1].BackgroundImage = Properties.Resources.black_highlited;
+                            board[x + 1, y + 1].BackgroundImage = Properties.Resources.black_highlited;
+
+                        }
+                        if (y == 0 && x < 7)
+                        {
+                            board[x + 1, y + 1].BackgroundImage = Properties.Resources.black_highlited;
+                        }
+                        if (y == 7 && x < 7)
+                        {
+                            board[x + 1, y - 1].BackgroundImage = Properties.Resources.black_highlited;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                history.Text+="MISS\n";
+            }             
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void stepper()
+        {
+
+        }
+    }
+}
