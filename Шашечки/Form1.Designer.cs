9f6d026817ba8653bfffc7882d7b514a92083aa2
Uselessness <Uselessness@USELESSNOTEBOOK>
2017-03-28 22:41:35 +0400

    фыв

--- Шашечки/Form1.Designer.cs
+++ Шашечки/Form1.Designer.cs
@@ -386,7 +386,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 30000;
+            this.timer1.Interval = 10000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
